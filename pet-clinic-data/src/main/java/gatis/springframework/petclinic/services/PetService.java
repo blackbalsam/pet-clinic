727f34686561d40beb9458900b615d9a0ee4b9e6
Gatis Jermacans <gatis.jermacans@gmail.com>
2020-04-12 22:35:26 +0300

    Implemented Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/gatis/springframework/petclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package gatis.springframework.petclinic.services;
+
+import gatis.springframework.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/gatis/springframework/petclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package gatis.springframework.petclinic.services;
+
+import gatis.springframework.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
