10ccf87da398a88c1541c24824c2d2a8ce231bf5
Gatis Jermacans <gatis.jermacans@gmail.com>
2020-05-05 15:57:51 +0300

    Added SpecialityService. Closes #32.

---
+++ pet-clinic-data/src/main/java/gatis/springframework/petclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package gatis.springframework.petclinic.services;
+
+import gatis.springframework.petclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality,Long> {
+}
