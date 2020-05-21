ddf6305866c8cbabb1d04e3e24850ef2a2f6d389
Gatis Jermacans <gatis.jermacans@gmail.com>
2020-05-21 10:52:27 +0300

    Converted owner to jpa entity. Closes #37

--- pet-clinic-data/src/main/java/gatis/springframework/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/gatis/springframework/petclinic/model/Pet.java
@@ -1,12 +1,21 @@
 package gatis.springframework.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
+    @Column(name = "birth_date")
     private LocalDate birthDate;
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
 
     public String getName() {
