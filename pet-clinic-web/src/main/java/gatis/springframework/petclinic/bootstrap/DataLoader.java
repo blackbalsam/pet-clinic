cd40276103223598e5bcf90792d1ed48cca31b13
Gatis Jermacans <gatis.jermacans@gmail.com>
2020-06-01 20:28:26 +0300

    Implemented JPA Visit Service, added loader data. Closes #48

--- pet-clinic-web/src/main/java/gatis/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/gatis/springframework/petclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package gatis.springframework.petclinic.bootstrap;
 
 import gatis.springframework.petclinic.model.*;
-import gatis.springframework.petclinic.services.OwnerService;
-import gatis.springframework.petclinic.services.PetTypeService;
-import gatis.springframework.petclinic.services.SpecialityService;
-import gatis.springframework.petclinic.services.VetService;
+import gatis.springframework.petclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -20,12 +17,14 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialityService specialityService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialityService = specialityService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -90,6 +89,13 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(fionasCat);
 
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+
+        visitService.save(catVisit);
 
         LOGGER.info("Loaded Owners...");
 
